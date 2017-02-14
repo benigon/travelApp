34532969b45207d70c17ace403dc5ed0d6ad1e86
Benito Gonzalez <bgonzalez@aminocom.com>
2017-02-14 17:01:31 +0000

    Updated jade files for pug files
    Starting authentication users

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,38 @@ var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 
- router.get('/trips', function(req, res) {
- 	  var MongoClient = mongodb.MongoClient;
- 	  var url = 'mongodb://localhost:27017/travelApp';
+router.get('/trips', function(req, res) {
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/travelApp';
 
- 	  MongoClient.connect(url, function(err, db) {
- 	  	if (err) {
- 	  		console.log("Unable to connect to the server", err);
- 	  	} else {
- 	  		console.log("Connetion established");
- 	  		var collection = db.collection('trip');
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log("Unable to connect to the server", err);
+		} else {
+			console.log("Connetion established");
+			var collection = db.collection('trip');
 
- 	  		collection.find({}).toArray(function(err, result){
- 	  			if (err) {
- 	  				res.send(err);
- 	  			} else if (result.length) {
- 	  				res.render('trip_list', {"tripList": result});
- 	  			} else {
- 	  				res.send("No documents found");
- 	  			}
-
- 	  			db.close();
+			collection.find({}).toArray(function(err, result){
+				if (err) {
+					res.send(err);
+				} else if (result.length) {
+					res.render('trip_list', {"tripList": result});
+				} else {
+					res.send("No documents found");
+				}
  	  		});
- 	  	}
+			db.close();
+		}
+	});
+});
 
- 	  });
- });
-
- router.get('/newtrip', function(req, res) {
- 	res.render('new_trip', {title: 'Add trip'});
- });
+router.get('/newtrip', function(req, res) {
+	res.render('new_trip', {title: 'Add trip'});
+});
 
 router.post('/addtrip', function(req, res) {
 	 var MongoClient = mongodb.MongoClient;
@@ -55,11 +53,25 @@ router.post('/addtrip', function(req, res) {
 	 			if (err) {
 	 				console.log(err);
 	 			} else {
-	 				res.redirect("trips");
+	 				res.redirect(303, "trips");
 	 			}
 	 			db.close();
 	 		});
 	 	}
 	 });
 });
+
+router.get('/headers', function(req, res) {
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for (var name in req.headers)
+		s += name + ": " + req.headers[name] + '\n';
+	res.send(s);
+})
+
+router.use(function(req, res, next){
+	res.status(404).render('error', {"message": "Page not found", "error": {"status": 404}});
+});
+
+
 module.exports = router;
